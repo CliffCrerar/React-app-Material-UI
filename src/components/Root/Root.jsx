838e268b36ade7f74f1a5c717f20651707cdb650
Cliff <cliff.crerar@gmail.com>
2019-05-30 00:59:20 +0200

    to do some pretty making of the boostrap app first time appearance

--- src/components/Root/Root.jsx
+++ src/components/Root/Root.jsx
@@ -1,12 +1,14 @@
-import React, { PureComponent } from 'react';
-import { withTheme } from '@material-ui/core/styles';
+import React, {PureComponent} from 'react';
+import {withTheme} from '@material-ui/core/styles';
+import {Paper} from '@material-ui/core';
 //import PropTypes from 'prop-types';
+//import { Test } from './Root.styles';
 
-//import { Test } from './Home.styles';
+import './Root.styles.css'
 
 class Root extends PureComponent {
-	constructor (props) {
-		super(props);
+	constructor ( props ) {
+		super( props );
 
 		this.state = {
 			hasError: false,
@@ -14,15 +16,15 @@ class Root extends PureComponent {
 	}
 
 	componentDidMount = () => {
-		console.log('Home mounted');
+		console.log( 'Home mounted' );
 	}
 
-	static getDerivedStateFromError(error) {
+	static getDerivedStateFromError( error ) {
 		// getDerivedStateFromError -> Update state so the next render will show the fallback UI.
-		return { hasError: true };
+		return {hasError: true};
 	}
 
-	componentDidCatch(error, info) {
+	componentDidCatch( error, info ) {
 		// You can also log the error to an error reporting service
 	}
 
@@ -35,20 +37,31 @@ class Root extends PureComponent {
 	// }
 
 	componentDidUpdate = () => {
-		console.log('Root did update');
+		console.log( 'Root did update' );
 	}
 
 	componentWillUnmount = () => {
-		console.log('Root will unmount');
+		console.log( 'Root will unmount' );
 	}
 
 	render() {
-		if (this.state.hasError) {
+		if ( this.state.hasError ) {
 			return <h1>Something went wrong.</h1>;
 		}
 		return (
 			<div className="HomeWrapper">
-				Test content
+				<Paper className="heading">
+					<h1 >Create React App integreted Material</h1>
+					<div>
+						<div>
+							<img />
+						</div>
+						<div>
+							<img />
+						</div>
+					</div>
+				</Paper>
+
 			</div>
 		);
 	}
@@ -62,4 +75,4 @@ Root.defaultProps = {
 	// bla: 'test',
 };
 
-export default withTheme()(Root);
+export default withTheme()( Root );
